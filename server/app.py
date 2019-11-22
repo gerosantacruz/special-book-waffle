c58fbbb9e6f831ac161e2d17a9099e3adea19abb
gerosantacruz <geronimo.santacruz2017@gmail.com>
2019-11-22 11:52:45 -0300

    We able to connect the backend and frontend, we can see the books

--- server/app.py
+++ server/app.py
@@ -18,6 +18,31 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 def ping_pong():
     return jsonify('pong!')
 
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({
+        'status':'success',
+        'books':BOOKS
+    })
+
+BOOKS = [
+    {
+        'title': 'Don quijote de la mancha',
+        'author': 'Cervantes',
+        'read':True
+    },
+    {
+        'title': 'La Iliada',
+        'author': 'Homero',
+        'read':True
+    },
+    {
+        'title': 'El arte de la guerra',
+        'author': 'Sun Tzu',
+        'read':True
+    }
+
+]
 
 if __name__ == '__main__':
     app.run()
