e6465d6cd995bed01a5ecb864f4f22d22750a833
gerosantacruz <geronimo.santacruz2017@gmail.com>
2019-11-23 16:35:09 -0300

    The update routhe was completed and working

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -1,72 +1,135 @@
 <template>
-    <!-- eslint-disable max-len -->
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-10">
-                <h1>Books</h1>
-                <hr><br><br>
-                <alert :message="message" v-if="showMessage"></alert>
-                <button type="button" class="btn btn-success btn-sm" v-b-modal.book-modal>Add Books</button>
-                <br><br>
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">Title</th>
-                            <th scope="col">Author</th>
-                            <th scope="col">Read?</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(book,index) in books" :key="index">
-                            <td>{{book.title}}</td>
-                            <td>{{book.author}}</td>
-                            <td>
-                                <span v-if="book.read">Yes</span>
-                                <span v-else>No</span>
-                            </td>
-                            <td>
-                                <div class="btn-toolbar" role="group">
-                                    <button type="button" class="btn btn-warning btn-sm btn-space">Update</button>
-                                    <button type="button" class="btn btn-danger btn-sm ">Delete</button>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <b-modal ref="addBookModal" id="book-modal" title="add a new book" hide-footer>
-          <b-form class="w-100" @submit="onSubmit" @reset="onReset">
-            <b-form-group id="form-title-group" label="Title:" label-for="form-title-input">
-              <b-form-input id="form-title-input" type="text" v-model="addBookForm.title" placeholder="Enter Title" required>
-              </b-form-input>
-            </b-form-group>
-            <b-form-group id="form-author-group" label="Author:" label-for="form-author-input">
-              <b-form-input id="form-author-input" type="text" v-model="addBookForm.author" placeholder="Enter Author" required>
-              </b-form-input>
-            </b-form-group>
-            <b-form-group id="form-read-group">
-              <b-form-checkbox-group v-model="addBookForm.read" id="form-checks">
-                <b-form-checkbox value="true">Read?</b-form-checkbox>
-              </b-form-checkbox-group>
-              <b-button type="submit" variant="primary" class="btn btn-space">Submit</b-button>
-              <b-button type="reset" variant="danger" class="btn btn-space">Reset</b-button>
-            </b-form-group>
-          </b-form>
-        </b-modal>
+  <!-- eslint-disable max-len -->
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-10">
+        <h1>Books</h1>
+        <hr />
+        <br /><br />
+        <alert :message="message" v-if="showMessage"></alert>
+        <button type="button" class="btn btn-success btn-sm" v-b-modal.book-modal>Add Books</button>
+        <br /><br />
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Author</th>
+              <th scope="col">Read?</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(book, index) in books" :key="index">
+              <td>{{ book.title }}</td>
+              <td>{{ book.author }}</td>
+              <td>
+                <span v-if="book.read">Yes</span>
+                <span v-else>No</span>
+              </td>
+              <td>
+                <div class="btn-toolbar" role="group">
+                  <button
+                    type="button"
+                    class="btn btn-warning btn-sm btn-space"
+                    v-b-modal.book-update-modal
+                    @click="editBook(book)"
+                  >
+                    Update
+                  </button>
+                  <button type="button" class="btn btn-danger btn-sm ">Delete</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+    <!-- Edit modal -->
+    <b-modal ref="addBookModal" id="book-modal" title="add a new book" hide-footer>
+      <b-form class="w-100" @submit="onSubmit" @reset="onReset">
+        <b-form-group id="form-title-group" label="Title:" label-for="form-title-input">
+          <b-form-input
+            id="form-title-input"
+            type="text"
+            v-model="addBookForm.title"
+            placeholder="Enter Title"
+            required
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-author-group" label="Author:" label-for="form-author-input">
+          <b-form-input
+            id="form-author-input"
+            type="text"
+            v-model="addBookForm.author"
+            placeholder="Enter Author"
+            required
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-read-group">
+          <b-form-checkbox-group v-model="addBookForm.read" id="form-checks">
+            <b-form-checkbox value="true">Read?</b-form-checkbox>
+          </b-form-checkbox-group>
+          <b-button type="submit" variant="primary" class="btn btn-space">Submit</b-button>
+          <b-button type="reset" variant="danger" class="btn btn-space">Reset</b-button>
+        </b-form-group>
+      </b-form>
+    </b-modal>
+
+    <!-- Edit modal -->
+    <b-modal ref="editBookModal" id="book-update-modal" title="Update" hide-footer>
+      <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+        <b-form-group id="form-title-edit-group" label="Title:" label-for="form-title-edit-input">
+          <b-form-input
+            id="form-title-edit-input"
+            type="text"
+            v-model="editForm.title"
+            required="required"
+            placeholder="Enter Title"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-title-edit-group" label="Author:" label-for="form-author-edit-input">
+          <b-form-input
+            id="form-author-edit-input"
+            type="text"
+            v-model="editForm.author"
+            required="required"
+            placeholder="Enter Author"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-read-edit-group">
+          <b-form-checkbox-group v-model="editForm.read" id="form-checks">
+            <b-form-checkbox value="true">Read?</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <b-button-group>
+          <b-button type="submit" variant="primary">Update</b-button>
+          <b-button type="reset" variant="danger">Cancel</b-button>
+        </b-button-group>
+      </b-form>
+    </b-modal>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
 import Alert from './Alert.vue';
 
-
 export default {
   data() {
     return {
       books: [],
       addBookForm: {
+        title: '',
+        author: '',
+        read: [],
+      },
+      message: '',
+      showMessage: false,
+      editForm: {
+        id: '',
         title: '',
         author: '',
         read: [],
@@ -79,7 +142,8 @@ export default {
   methods: {
     getBooks() {
       const path = 'http://localhost:5000/books';
-      axios.get(path)
+      axios
+        .get(path)
         .then((res) => {
           this.books = res.data.books;
         })
@@ -90,7 +154,8 @@ export default {
     },
     addBook(payload) {
       const path = 'http://localhost:5000/books';
-      axios.post(path, payload)
+      axios
+        .post(path, payload)
         .then(() => {
           this.getBooks();
           this.message = 'Book added!';
@@ -106,6 +171,10 @@ export default {
       this.addBookForm.title = '';
       this.addBookForm.author = '';
       this.addBookForm.read = [];
+      this.editForm.id = '';
+      this.editForm.title = '';
+      this.editForm.author = '';
+      this.editForm.read = [];
     },
     onSubmit(evt) {
       evt.preventDefault();
@@ -125,17 +194,41 @@ export default {
       this.$refs.addBookModal.hide();
       this.initForm();
     },
-    data() {
-      return {
-        books: [],
-        addBookForm: {
-          title: '',
-          author: '',
-          read: [],
-        },
-        message: '',
-        showMessage: false,
+    editBook(book) {
+      this.editForm = book;
+    },
+    onSubmitUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      let read = false;
+      if (this.editForm.read[0]) read = true;
+      const payload = {
+        title: this.editForm.title,
+        author: this.editForm.author,
+        read,
       };
+      this.updateBook(payload, this.editForm.id);
+    },
+    updateBook(payload, bookID) {
+      const path = `http://localhost:5000/books/${bookID}`;
+      axios
+        .put(path, payload)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Book Updated!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getBooks();
+        });
+    },
+    onResetUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      this.initForm();
+      this.getBooks();
     },
   },
   created() {
